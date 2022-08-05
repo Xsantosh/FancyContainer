efae89a56bbf5e2aa98a473e9dffde3d4c047b2d
santosh <xsantosh7@gmail.com>
2022-08-05 17:09:34 +0530

    chnages file name and library name

---
+++ lib/gradient_container.dart
@@ -0,0 +1,4 @@
+library gradient_container;
+
+export 'src/gradient_container_new.dart';
+
---
+++ test/gradient_container_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gradient_container/gradient_container.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
